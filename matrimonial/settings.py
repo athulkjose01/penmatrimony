8c1e914005d0d6704026983f0da0e1150568c62e
athulkjose01 <athulkjose001@gmail.com>
2025-06-15 21:31:20 +0530

    Initial committtt

--- matrimonial/settings.py
+++ matrimonial/settings.py
@@ -34,7 +34,9 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
+# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ['*']
 
 
 dotenv_path = BASE_DIR / '.env' # Add this
@@ -84,6 +86,15 @@ TEMPLATES = [
         },
     },
 ]
+
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://pentecostmatrimony.co.in',
+    'https://www.pentecostmatrimony.co.in',
+]
+
+
 
 
 
