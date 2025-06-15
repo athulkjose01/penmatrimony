cda992a31029f261723f54d1a791ceaac216330b
athulkjose01 <athulkjose001@gmail.com>
2025-06-15 20:50:32 +0530

    Initial committ

--- matrimonial/asgi.py
+++ matrimonial/asgi.py
@@ -1,16 +1,19 @@
-# matrimonial/asgi.py
-
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import matri.routing # 'matri' is your app name, this should be correct
 
-# This line is now fixed
+# This line MUST be at the top
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matrimonial.settings')
 
+# This line is the KEY. It initializes Django *before* other imports.
+django_asgi_app = get_asgi_application() 
+
+# Now that Django is ready, we can safely import our app's code.
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import matri.routing
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app, # Use the app instance we already created
     "websocket": AuthMiddlewareStack(
         URLRouter(
             matri.routing.websocket_urlpatterns
