e64bc8bbbb29f5387aa8bf164382f06a434626b6
athulkjose01 <athulkjose001@gmail.com>
2025-06-25 22:18:23 +0530

    Initial committt

--- matrimonial/settings.py
+++ matrimonial/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = config('DEBUG', default=False, cast=bool)
 
-DEBUG = False
+DEBUG = True
 
 
 # ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
